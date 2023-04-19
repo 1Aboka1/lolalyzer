e385aa2641f8b21e55086e797960dca66f07a87f
Abulkhair <zz.abulhair.zz@gmail.com>
2023-04-19 19:13:56 +0600

    adding setters

--- .venv/bin/activate.csh
+++ .venv/bin/activate.csh
@@ -8,7 +8,7 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV "/home/aboka/Code/Python/TelegramBot"
+setenv VIRTUAL_ENV "/home/aboka/Code/Python/TelegramBot/.venv"
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/bin:$PATH"
@@ -17,8 +17,8 @@ setenv PATH "$VIRTUAL_ENV/bin:$PATH"
 set _OLD_VIRTUAL_PROMPT="$prompt"
 
 if (! "$?VIRTUAL_ENV_DISABLE_PROMPT") then
-    set prompt = "(TelegramBot) $prompt"
-    setenv VIRTUAL_ENV_PROMPT "(TelegramBot) "
+    set prompt = "(.venv) $prompt"
+    setenv VIRTUAL_ENV_PROMPT "(.venv) "
 endif
 
 alias pydoc python -m pydoc
--- main.py
+++ main.py
@@ -25,7 +25,7 @@ regions = {
 # Needs to be regenerated every 24 hours
 riot_TOKEN = "RGAPI-f885ae8c-080f-4dc0-b4d1-bfa54b5a446d"
 watcher = LolWatcher(riot_TOKEN)
-region = 'ru'
+region = None
 
 bot_TOKEN = "5847363406:AAHY3CnYL2Pmwbl7qWZ2hybz-JmV0sa6ocI"
 bot = telebot.TeleBot(bot_TOKEN, parse_mode=None)
@@ -54,6 +54,7 @@ def send_welcome(message):
     bot.register_callback_query_handler(sent_msg, summoner_region_handler)
 
 def summoner_region_handler(callback):
+    global region
     region = callback.data
 
     sent_msg = bot.send_message(callback.from_user.id, "Enter the Summoner's Name: 🧙🏽‍♂️", parse_mode="Markdown")
@@ -61,31 +62,42 @@ def summoner_region_handler(callback):
 
 def summoner_search_handler(message):
     summoner_name = message.text 
-    summoner = watcher.summoner.by_name(region, summoner_name)
-    sent_msg = bot.send_message(message.chat.id, gen_summoner_summary(summoner))
+    summoner = Summoner(watcher.summoner.by_name(region, summoner_name))
+    sent_msg = bot.send_message(message.chat.id, summoner.gen_summoner_summary())
 
-    gen_more_options(message)
+    summoner.gen_more_options(message)
 
-def gen_more_options(message):
-    keyboard = [[InlineKeyboardButton('Summoner\'s Rank', callback_data='summoner_ranking_request'),
-                InlineKeyboardButton('Last Game Info', callback_data='last_game_info_request')]]
-    markup = InlineKeyboardMarkup(keyboard)
-    markup.row_width = 2
+class Summoner:
+    def __init__(self):
+        pass
 
-    sent_msg = bot.send_message(message.chat.id, "What would you want to know about the Summoner❓", reply_markup=markup)
-    bot.register_callback_query_handler(sent_msg, process_more_request)
-    
-def process_more_request(callback):
-    if callback.data == 'summoner_ranking_request':
-        print(summoner)
+    def set_region(self):
+        pass
 
-    elif callback.data == 'last_game_info_request':
+    def set_summoner(self):
+        pass
 
-def gen_summoner_summary(summoner):
-    text = ''
-    if summoner:
-        text += '🧙🏻‍♂️ Summoner\'s name: ' + str(summoner['name']) + '\n'
-        text += '🌡️Summoner\'s level: ' + str(summoner['summonerLevel']) + '\n'
-    return text         
+    def gen_more_options(self, message):
+        keyboard = [[InlineKeyboardButton('Summoner\'s Rank', callback_data='summoner_ranking_request'),
+                    InlineKeyboardButton('Last Game Info', callback_data='last_game_info_request')]]
+        markup = InlineKeyboardMarkup(keyboard)
+        markup.row_width = 2
+
+        sent_msg = bot.send_message(message.chat.id, "What would you want to know about the Summoner❓", reply_markup=markup)
+        bot.register_callback_query_handler(sent_msg, self.process_more_request)
+        
+    def gen_summoner_summary(self):
+        text = ''
+        text += '🧙🏻‍♂️ Summoner\'s name: ' + str(self.summoner_data['name']) + '\n'
+        text += '🌡️Summoner\'s level: ' + str(self.summoner_data['summonerLevel']) + '\n'
+        return text         
+
+    def process_more_request(self, callback):
+        if callback.data == 'summoner_ranking_request':
+            self.ranked_stats = watcher.league.by_summoner(region, self.summoner_data['id'])
+            print(self.ranked_stats)
+
+        elif callback.data == 'last_game_info_request':
+            print(self.summoner_data)
 
 bot.infinity_polling(interval=0, timeout=20)
